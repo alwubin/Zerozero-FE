686db6fc1042117426f7744ee2819dc3021e553c
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-01-16 01:18:31 +0900

    [feat] : 하단 바 클릭 이벤트 추가

--- src/App.js
+++ src/App.js
@@ -8,10 +8,23 @@ import { BiComment } from "react-icons/bi";
 import { GiSodaCan } from "react-icons/gi";
 import { FaMapPin } from "react-icons/fa";
 import { AiOutlineUser } from "react-icons/ai";
+import { useState } from 'react';
 
 
 function App() {
   const navigate = useNavigate();
+  const [isClicked, setIsClicked] = useState('/');
+
+  const clicked = {
+    backgroundColor:'#fffafa',
+    color: '#CD5C5C',
+  }
+
+  const unClicked = {
+    backgroundColor:'#fffafa',
+    color: '#262626',
+  }
+
   return (
     <div className="App">
       <Routes>
@@ -23,15 +36,43 @@ function App() {
       </Routes>
 
       <div className='servicesBox'>
-        <div className='chatService' onClick={() => {navigate('/chat')}}>
-          <div className='chatIcon'><BiComment style={{fontSize:'27px'}}/><GiSodaCan style={{zIndex:'1001', fontSize:'12px', position:'absolute', bottom:'33px', left:'104px'}}/></div>
-          채팅방 </div>
-        <div className='mapService' onClick={() => {navigate('/')}}>
-          <div className='mapIcon'><FaMapPin style={{fontSize:'23px', paddingTop:'2px'}}/></div>
-          메인</div>
-        <div className='loginService' onClick={() => {navigate('/login')}}>
-          <div className='loginIcon'><AiOutlineUser style={{fontSize:'27px'}}/></div>
-          로그인</div>
+        <div className='chatService' 
+          onClick={() => {
+            navigate('/chat');
+            setIsClicked('/chat');
+          }}
+          style={isClicked === '/chat' ? clicked : unClicked}>
+          <div className='chatIcon'>
+            <BiComment style={{fontSize:'27px'}}/>
+            <GiSodaCan style={{zIndex:'1001', fontSize:'12px', position:'absolute', bottom:'33px', left:'104px'}}/>
+          </div>
+          채팅방 
+        </div>
+
+        <div className='mapService' 
+          onClick={() => {
+            navigate('/');
+            setIsClicked('/');
+          }}
+          style={isClicked === '/' ? clicked : unClicked}>
+          <div className='mapIcon'>
+            <FaMapPin style={{fontSize:'23px', paddingTop:'2px'}}/>
+          </div>
+          메인
+        </div>
+
+        <div className='loginService' 
+          onClick={() => {
+            navigate('/login');
+            setIsClicked('/login');
+          }}
+          style={isClicked === '/login' ? clicked : unClicked}>
+          <div className='loginIcon'>
+            <AiOutlineUser style={{fontSize:'27px'}}/>
+          </div>
+          로그인
+        </div>
+        
       </div>
     </div>
   );
