f463c1b1c8b8f426805c8cac58b3c5d8b8a58444
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-01-16 00:22:47 +0900

    [feat] : 회원가입 기능 구현 및 api 연결

--- src/routes/SignUp.js
+++ src/routes/SignUp.js
@@ -11,22 +11,56 @@ function SignUp() {
     const [nickname, setNickname] = useState('');
 
     //duplicate for email password nickname
-    const [emailDuplicate, setEmailDuplicate] = useState(false);
+    const [emailDuplicate, setEmailDuplicate] = useState(true);
     const [passwordValid, setPasswordValid] = useState(false);
     const [passwordMatch, setPasswordMatch] = useState(false);
-    const [nicknameDuplicate, setNicknameDuplicate] = useState(false);
-    const [notAllow, setNotAllow] = useState(true);
+    const [nicknameDuplicate, setNicknameDuplicate] = useState(true);
+    const [isEmpty, setIsEmpty] = useState(true);
+    const [emailNotAllow, setEmailNotAllow] = useState(false);
+    const [nicknameNotAllow, setNicknameNotAllow] = useState(true);
+   
+    const registerUser = () => {
+        axios.post('http://ec2-3-35-98-32.ap-northeast-2.compute.amazonaws.com:8080/api/v1/auth/register', 
+            {
+                'nickname': nickname,
+                'email': `${email}@${emailOpt}`,
+                'password': password
+            },
+            { withCredentials: true }
+        )
+        .then((res) => {
+            console.log(res);
+            alert('회원가입 성공');
+            window.location.href = '/login';
+        })
+        .catch((err) => {
+            console.log(err);
+            alert('회원가입 실패');
+        })
+    }
 
     const checkEmailDuplication = () => {
-        axios.get(`http://ec2-3-35-98-32.ap-northeast-2.compute.amazonaws.com:8080/api/v1/auth/check-email/${email}%40${emailOpt.value}`)
+        axios.get(`http://ec2-3-35-98-32.ap-northeast-2.compute.amazonaws.com:8080/api/v1/auth/check-email/${encodeURIComponent(email+'@'+emailOpt)}`, { withCredentials: true })
         .then((res) => {
             console.log(res);
         })
         .catch(() => {
             console.log('이미 존재하는 이메일 입니다.');
-            setEmailDuplicate(true);
+            setEmailDuplicate(false);
         })
-        setEmailDuplicate(false);
+        setEmailDuplicate(true);
+    }
+
+    const checkNicknameDuplication = () => {
+        axios.get(`http://ec2-3-35-98-32.ap-northeast-2.compute.amazonaws.com:8080/api/v1/auth/check-nickname/${encodeURIComponent(nickname)}`, { withCredentials: true })
+        .then((res) => {
+            console.log(res);
+        })
+        .catch(() => {
+            console.log('이미 존재하는 닉네임 입니다.');
+            setNicknameDuplicate(false);
+        })
+        setNicknameDuplicate(true);
     }
 
     const handleEmail = (e) => {
@@ -35,16 +69,12 @@ function SignUp() {
 
     const handleEmailOpt = (e) => {
         setEmailOpt(e.target.value);
-
-        if (e.target.value === 'type') {
-            document.getElementsByName('emailSelected')[0].value = '';
-        }
-    }
+    };
 
     const handlePassword = (e) => {
         setPassword(e.target.value);
         const regex = 
-            /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
+        /^.*(?=^.{7,20}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
         if(regex.test(password)) {
             setPasswordValid(true);
         } else {
@@ -54,22 +84,52 @@ function SignUp() {
 
     const handlePasswordCheck = (e) => {
         setPasswordCheck(e.target.value);
-        if (password !== passwordCheck) {
+    }
+
+    const handleNickname = (e) => {
+        setNickname(e.target.value);
+    }
+
+    useEffect(() => {
+        if(password === passwordCheck) {
             setPasswordMatch(true);
         } else {
             setPasswordMatch(false);
         }
-    }
-
+    })
 
     useEffect(() => {
         const regex = 
         /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
         
-        if (regex.test(email)) {
-            setNotAllow(false);
+        if (regex.test(email + '@' + emailOpt)) {
+            setEmailNotAllow(false);
         } else {
-            setNotAllow(true);
+            setEmailNotAllow(true);
+        }
+    })
+
+    //공백 없는 숫자 한글 문자
+    useEffect(() => {
+        const regex = /^[ㄱ-ㅎ가-힣a-zA-Z0-9]{2,10}$/;
+        if (regex.test(nickname)) {
+            setNicknameNotAllow(false);
+        } else {
+            setNicknameNotAllow(true);
+        }
+    })
+
+    useEffect(() => {
+        if (
+            email === '' || 
+            emailOpt === '' || 
+            password === '' || 
+            passwordCheck === '' || 
+            nickname === ''
+            ) {
+            setIsEmpty(true);
+        } else {
+            setIsEmpty(false);
         }
     })
 
@@ -101,27 +161,26 @@ function SignUp() {
                             value={email}
                             onChange={handleEmail}
                         /> @ &nbsp; 
-                        {emailOpt === 'type' ? (
-                            <input
-                                type='text'
-                                className='signUpEmailInput'
-                                name='emailSelected'/>
-                            ) : (
-                            <input
+                        {/* <input
+                            type='text'
+                            className='signUpEmailInput'
+                            name='emailSelected'
+                            value={emailOpt}
+                            onChange={handleEmailOpt}
+                            /> */}
+                        <input
                                 type='text'
                                 className='signUpEmailInput'
                                 name='emailSelected'
                                 value={emailOpt}
                                 readOnly/>
-                            )
-                        }
                         <select
                             className='signUpOptInput'
                             name='emailOpt'
                             id='emailOpt'
                             onChange={handleEmailOpt}
                             value={emailOpt}>
-                            <option value='type'>직접 입력</option>
+                            <option disabled value=''>-- 선택 --</option>
                             <option value='naver.com'>naver.com</option>
                             <option value='gmail.com'>gmail.com</option>
                             <option value='hanmail.net'>hanmail.net</option>
@@ -131,7 +190,7 @@ function SignUp() {
                             <option value='kakao.com'>kakao.com</option>
                         </select>
                     </form>
-                    <button className='checkDuplicateButton' disabled={notAllow} onClick={checkEmailDuplication}>중복확인</button>
+                    <button className='checkDuplicateButton' disabled={emailNotAllow} onClick={checkEmailDuplication}>중복확인</button>
                     
                 </div>
 
@@ -160,7 +219,7 @@ function SignUp() {
                     <div className='signUpInputTitle'>비밀번호 확인</div>
                     <div className='signUpErrorMsgWrap'>
                         {
-                            !passwordMatch && (
+                            !passwordMatch && passwordCheck.length > 0 && (
                                 <div>비밀번호가 일치하지 않습니다.</div>
                             )
                         }
@@ -180,19 +239,25 @@ function SignUp() {
                 <div className='signUpInputTitleWrap'>
                     <div className='signUpInputTitle'>닉네임</div>
                     <div className='signUpErrorMsgWrap'>
-                        <div>사용하실 닉네임을 입력해주세요.</div>
+                        {
+                            !nicknameDuplicate && (
+                                <div>이미 존재하는 닉네임입니다.</div>
+                            )
+                        }
                     </div>
                 </div>
+
                 <div className='signUpInputWrap'>
                     <input
                         type='text' 
                         className='signUpInput' 
                         placeholder='닉네임 입력'
+                        onChange={handleNickname}
                     />
-                    <button className='checkDuplicateButton' disabled={true}>중복확인</button>
+                    <button className='checkDuplicateButton' disabled={nicknameNotAllow} onClick={checkNicknameDuplication}>중복확인</button>
                 </div>
 
-                <button className='joinButton'>가입하기</button>
+                <button className='joinButton' disabled={isEmpty} onClick={registerUser}>가입하기</button>
                 <button className='cancelButton'>취소</button>
             </div>
         </div>
