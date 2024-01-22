aeb08c97a9ba7f9936d7e67c45ec4ca5fad0923b
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-01-22 19:16:48 +0900

    [docs] : 기능에 따라 폴더 이동

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import './App.css';
-import Main from './routes/Main.js';
-import Chat from './routes/Chat.js';
-import Login from './routes/Login.js';
-import SignUp from './routes/SignUp.js';
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import Main from './pages/Main.js';
+import Chat from './pages/Chat.js';
+import Login from './pages/Login.js';
+import SignUp from './pages/SignUp.js';
+import MyPage from './pages/MyPage.js';
+
+import { Routes, Route, useNavigate, Navigate } from 'react-router-dom'
 import { BiComment } from "react-icons/bi";
 import { GiSodaCan } from "react-icons/gi";
 import { FaMapPin } from "react-icons/fa";
 import { AiOutlineUser } from "react-icons/ai";
 import { useState } from 'react';
 
+/**
+ * CHECKLIST
+ * [X] private route 분리 - 마이페이지
+ * [X] 로그인 상태에 따라 분리 로그인 페이지 또는 마이페이지로 이동
+ * [ ] 로그인 시 하단 바 로그인 버튼이 마이페이지 버튼으로 변경
+ */
+
 
 function App() {
-  const navigate = useNavigate();
   const [isClicked, setIsClicked] = useState('/');
+  const navigate = useNavigate();
+
+  const isAuthoenticated = () => {
+    const token = localStorage.getItem('token');
+  
+    return !!token; // 있으면 true, 없으면 false 반환
+  }
 
   const clicked = {
     backgroundColor:'#fffafa',
@@ -30,11 +45,19 @@ function App() {
       <Routes>
         <Route path="/" element={<Main/>} />
         <Route path="/chat" element={<Chat/>} />
-        <Route path="/login" element={<Login/>} />
+        <Route 
+            path="/login" 
+            element={ isAuthoenticated() ? <Navigate to="/mypage" /> : <Login/> } 
+        />
         <Route path="/signup" element={<SignUp/>} /> 
+        <Route 
+            path="/mypage" 
+            element={ isAuthoenticated() ? <MyPage/> : <Navigate to="/login" /> } 
+        />
         <Route path="*" element={<div>잘못된 접근입니다.</div>} /> 
       </Routes>
 
+      {/* Footer: 하단 바 */}
       <div className='servicesBox'>
         <div className='chatService' 
           onClick={() => {
