6a2227e19ae3edc0c9c8e5a7dd5508ad083cbedb
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-01-26 17:25:38 +0900

    [feat] : 검색 창에 도로명 주소를 입력받아 마커로 지도에 표시하기

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -28,51 +28,74 @@ const inputStyle = {
 
 function Main() {
     const navermaps = useNavermaps();
-    const navigate = useNavigate();
 
     const [address, setAddress] = useState('');
-    const [roadAddress, setRoadAddress] = useState(null);
 
-    const [lat, setLat] = useState(37.3595704);
-    const [lng, setLng] = useState(127.105399);
+    const [lat, setLat] = useState(37.3595704); //y
+    const [lng, setLng] = useState(127.105399); //x
+    const [storeName, setstoreName] = useState(''); //주소명
 
     const handleChange = (e) => {
         const newAddress = e.target.value
         setAddress(newAddress);
     }
 
-    //주소를 입력하면 위도, 경도 찾기 함수 호출
-    function searchAddress() {
-        //입력받은 주소를 query로 전달
+    function searchAddress(){
         navermaps.Service.geocode(
             {
-                query: '보정동 694',
+                query: address,
             },
-            function(status, response) {
-                //제대로 된 주소가 들어가지 않는 경우
+            function (status, response) {
                 if (status !== navermaps.Service.Status.OK) {
-                    return alert('주소를 찾을 수 없습니다.'); 
-                    console.log(status)
+                    console.log('error');
+                    return alert('주소를 찾을 수 없습니다.');
                 }
-                var result = response.v2, //검색 결과 컨테이너
-                    items = result.addresses; //검색 결과 배열
-                
-                setLat(parseFloat(items[0].y));
-                setLng(parseFloat(items[0].x));
-                setRoadAddress(items[0].roadAddress);
+
+                console.log('응답: ', response);
+    
+                const items = response.v2.addresses[0];
+                console.log('아이템: ', items);
+
+                let addressList = items.jibunAddress.split(' '); //지번 주소
+                // console.log('주소 리스트: ', addressList);
+    
+                for (let i = 0; i < addressList.length; i++) {
+                    if(!isNaN(+addressList[i])) break;
+                    addressList.splice(i, 1);
+                    i--;
+                }
+                for (let i = 0; i < addressList.length; i++) {
+                    if(isNaN(+addressList[i])) break;
+                    addressList.splice(i, 1);
+                    i--;
+                }
+
+                setstoreName([...addressList].join(' ')); //주소명
+                console.log('장소명: ', storeName);
+
+                console.log('위도: ', items.y, '경도: ', items.x);
+                setLng(items.x);
+                setLat(items.y);
             }
         );
     }
+
     
     return (
         <MapDiv style={mapStyle}>
-            <NaverMap defaultCenter={{ lat: lat, lng: lng }} defaultZoom={17} >
-                <Marker position={{ lat: lat, lng: lng }} />
+            <NaverMap 
+                // defaultCenter={{ lat: 37.3595704, lng: 127.105399 }}
+                center={{ lat: lat, lng: lng }} 
+            >
+                <Marker 
+                    // defaultPosition={{ lat: 37.3595704, lng: 127.105399 }}
+                    position={{ lat: lat, lng: lng }} 
+                />
                 <div className="contentWrap">
                     <div className="inputWrap">
                         <input 
                             style={inputStyle}
-                            placeholder="지역이나 장소를 검색해 보세요" 
+                            placeholder="지역이나 장소를 도로명 주소로 검색해 보세요" 
                             value={address} 
                             onChange={handleChange}/>
                     </div>
