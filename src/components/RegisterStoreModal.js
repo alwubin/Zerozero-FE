c87ee843a5d32781d30383573c7702f5b2426807
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-03-20 00:48:22 +0900

    [feat] : 판매점 등록 모달 기능 및 스타일 구현

--- src/components/RegisterStoreModal.js
+++ src/components/RegisterStoreModal.js
@@ -4,72 +4,78 @@ import axios from 'axios';
 import '../styles/RegisterStoreModal.css'
 
 const RegisterStoreModal = ({ request }) => {
+    const [images, setImages] = useState([]);
+
+    const handleImageUpload = (e) => {
+        const uploadImages = Array.from(e.target.files);
+        const selectedImages = uploadImages.slice(0,3);
+        setImages(selectedImages);
+    }
 
     const registerLocation = () => {
+        const formData = new FormData();
+        images.forEach((image, index) => {
+            formData.append(`image${index + 1}`, image);
+        });
+
+        const requestData = {
+            title: request.title,
+            mapx: request.mapx,
+            mapy: request.mapy,
+            address: request.address
+        };
+
+        formData.append('requestData', JSON.stringify(requestData));
+
         axios.post(`http://ec2-3-35-98-32.ap-northeast-2.compute.amazonaws.com:8080/api/v1/stores`, 
+        formData,
             { 
                 withCredentials: true,
                 headers: {
-                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                    'Content-Type': 'multipart/form-data'
                 } 
-            },
-            {
-                "request": {
-                  "title": request.title,
-                  "mapx": request.mapx,
-                  "mapy": request.mapy
-                },
-                "images": [
-                  "string"
-                ]
             }
         )
         .then((res) => {
-            
+            console.log('성공적으로 등록되었습니다!')
         })
         .catch((err) => {
             console.log(err);
         })
     }
+
+    const handleRegisterButtonClick = () => {
+        registerLocation();
+    }
+
     return (
-        <div className='contentWrapper'>
-            <div className='itemsWrapper'>
-                <div className='storeTitle'>
-                    <div>
-                        <h3>가게 이름</h3>
-                    </div>
-                    <div>
-                        {request.title}
-                    </div>
+        <div className='registerModal'>
+            <div className='registerContentWrap'>
+                <div className='infoTitle'>
+                    가게명 :
                 </div>
-                <div className='zreoBeverage'>
-                    <div>
-                        <h3>판매중인 제로 음료수</h3>
-                    </div>
-                    <div>
-                        <ul className='zeroList'>
-                            <li>코카콜라 제로</li>
-                            <li>펩시 제로</li>
-                            <li>스프라이트 제로</li>
-                            <li>칠성사이다 제로</li>
-                            <li>탐스 제로</li>
-                            <li>웰치스 제로</li>
-                            <li>환타 제로</li>
-                            <li>닥터페퍼 제로</li>
-                            <li>밀키스 제로</li>
-                        </ul>
-                    </div>
+                <div className='infoContent'>
+                    {request.title}
                 </div>
-                <div className='storeImage'>
-                    <div>
-                        <h3>가게 사진</h3>
-                        사진을 제보해주세요!
-                    </div>
-                    <div>
-                        
-                    </div>
+
+                <div className='infoTitle'>
+                    가게 주소 :
                 </div>
-                <button onClick={registerLocation}>가게 등록하기</button>
+                <div className='infoContent'>
+                    {request.address}
+                </div>
+
+                
+                <div className='infoTitle'>
+                    가게 사진
+                    <a>사진을 제보해주세요!</a>
+                </div>
+                <div className='imageContainer'>
+                    <input className='imageInput' type="file" accept="image/*" multiple onChange={handleImageUpload} />
+                </div>
+
+                <button className='registerStoreButton' onClick={handleRegisterButtonClick}>등록</button>
             </div>
         </div>
     )
