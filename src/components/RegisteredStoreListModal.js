58acce4660be7fad97e9b45f110d8c80c6c59358
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-03-26 01:49:51 +0900

    [feat] : 등록한 장소 모달 디자인 수정

--- src/components/RegisteredStoreListModal.js
+++ src/components/RegisteredStoreListModal.js
@@ -3,21 +3,27 @@ import '../styles/RegisteredStoreListModal.css';
 
 const RegisteredStoreListModal = ({ registeredStoreList, clickHandler }) => {
     return (
-        <div className="registeredStoreListWrap" onClick={clickHandler}>
-            {
-                registeredStoreList.map((store, idx) => (
-                    <div className='registeredStoreListContent' key={idx}>
-                        {/* <div className='storeId'> {idx + 1} </div> */}
-                        <div className='storeName'> {store.name} </div>
-                        <div className='storeAddress'>
-                            {store.roadAddress}
-                            <div> {store.category} </div>
+        <div className="registeredStoreListContainer">
+            <div className='registerStoreList'>
+                <div className="modalCloseButton" onClick={clickHandler}>
+                    X
+                </div>
+                { registeredStoreList.map((store, idx) => (
+                    <React.Fragment key={idx}>
+                        <div className='registeredStoreInfo'>
+                            <div className='registerStoreTitle'> {store.name} </div>
+                            <div className='registerStoreAddress'>
+                                {store.roadAddress}
+                                <div className='registerStoreCategory'> {store.category} </div>
+                            </div>
                         </div>
-                    </div>
-                ))
-            }
+                        {idx !== registeredStoreList.length - 1 && <hr />}
+                    </React.Fragment>
+                ))}
+            </div>
         </div>
-    )
-}
+    );
+};
+
 
 export default RegisteredStoreListModal;
